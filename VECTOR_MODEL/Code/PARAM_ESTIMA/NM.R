69f6d65048d02397c17ae62248b5cb07cfec092c
Marta Pardo <pardoaraujomarta92@gmail.com>
2021-09-14 16:27:28 +0200

    as

--- VECTOR_MODEL/Code/PARAM_ESTIMA/NM.R
+++ VECTOR_MODEL/Code/PARAM_ESTIMA/NM.R
@@ -298,7 +298,7 @@ round <- 1
 # sims <- 2 #Número de combinaciones paramétricas a explorar
 sims <- ncol(seeds) #Número de combinaciones paramétricas a explorar
 
-Cores <- 16 #Numero de cores a utilizar.
+Cores <- parallel::detectCores() #Numero de cores a utilizar.
 it <- 0
 while(condition){
   #Ahora viene la paralelización
