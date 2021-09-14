f6029cf076dd8501c30facd6e3609afbb740752d
Marta Pardo <pardoaraujomarta92@gmail.com>
2021-09-14 09:47:03 +0200

    asd

--- SUR_MODEL/Code/MH_50_CLC.R
+++ SUR_MODEL/Code/MH_50_CLC.R
@@ -225,7 +225,7 @@ run_metropolis_MCMC = function(startvalue, iterations){
 }
 
 startvalue = c(0.1,1,3.5,0.5)
-iterations = 10000
+iterations = 50000
 chain = run_metropolis_MCMC(startvalue, iterations)
 
 burnIn = 50
