8af0cc2fc66c500a7c6b86f094b544ec75a3d5a0
Marta Pardo <pardoaraujomarta92@gmail.com>
2021-09-22 10:39:13 +0200

    f

--- SUR_MODEL/Code/MH_2000_CLC.R
+++ SUR_MODEL/Code/MH_2000_CLC.R
@@ -5,47 +5,6 @@ library("parallel")
 library("tidyverse")
 library("deSolve")
 
-# Create Pseudo data:
-# Create Pseudo Data:
-# Path = "~/MAD_MODEL/SUR_MODEL/Code/"
-# # Path = paste(PC,Path, sep="")
-# 
-# setwd(Path)
-# system("R CMD SHLIB model_2000eq.c")
-# dyn.load("model_2000eq.so")
-# 
-# gam1 = 0.2
-# gam2 = 1.2
-# gam3 = 3
-# # We create a vector with the constant parameters.
-# parms = c(gam1,gam2,gam3)
-# # We set the initial conditions to zero.
-# Y <-  matrix(0, nrow = 1, ncol=2000)
-# Path = "~/MAD_MODEL/SUR_MODEL/data/Downloads_2378.data"
-# down <- data.frame(t(read.table(Path, header=FALSE)))
-# colnames(down) <- c("time", "down")
-# 
-# # List with the data frames of the forcings, sort as the c code.
-# forcs_mat <- list(data.matrix(down))
-# 
-# min_t <- min(down$time)
-# max_t <- max(down$time)
-# times <- seq(min_t,max_t, 1)
-# 
-# out <- ode(Y, times, func = "derivs",
-#            parms = parms, dllname = "model_2000eq",
-#            initfunc = "initmod", nout = 1,
-#            outnames = "Sum", initforc = "forcc",
-#            forcings = down, 
-#            fcontrol = list(method = "constant")) 
-# 
-# 
-# ode <- data.frame(out)
-# ode$Sum <- NULL
-# head(ode)
-# ode_mat <- as.matrix(ode)
-# saveRDS(ode, file = "~/MAD_MODEL/SUR_MODEL/Code/ode_pseudo_2000eq.rds")
-
 # Función de c que corre la ODE -------------------------------------------
 
 # Esta función está adaptada para su uso con el paquete deSolve, el estándar en
@@ -114,23 +73,6 @@ forcs_mat <- data.matrix(down)
 # Read Observed data:
 ob_data <-read.table("~/MAD_MODEL/SUR_MODEL/Code/Observed_data_2300.data", header=FALSE, sep= " ")
 ob_data <- t(ob_data[,1:2000])
-# 
-# # Pseudo Data to check the oprimization method.
-# ob_data <- readRDS(file = "~/MAD_MODEL/SUR_MODEL/Code/ode_pseudo_2000eq.rds")
-# l <- nrow(ob_data)
-# # ob_data[,2:1001] <- ob_data[,2:1001] + matrix(rnorm(l*2000,0,trueSD), ncol = 2000, nrow = l)
-# 
-# 
-# head(ob_data)
-# # Example: plot the likelihood profile of the slope.
-# slopevalues <- function(par){
-#   return(likelihood(ob_data,c(par, true2,true3, trueSD),forcs_mat))
-# } 
-# 
-# vec <- seq(0, 1, by=.1)
-# slopelikelihoods <- lapply(vec, slopevalues )
-# 
-# plot(vec, slopelikelihoods , type="l", xlab = "values of slope gamma 1", ylab = "Log likelihood")
 
 # Prior distribution
 prior = function(param){
@@ -192,31 +134,7 @@ burnIn = 50
 
 acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
 print(paste0("Acceptance rate: ", acceptance))
-### Summary: #######################
-# 
-# par(mfrow = c(2,4))
-# hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of a", xlab="True value = red line" )
-# abline(v = mean(chain[-(1:burnIn),1]))
-# abline(v = true1, col="red" )
-# hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of b", xlab="True value = red line")
-# abline(v = mean(chain[-(1:burnIn),2]))
-# abline(v = true2, col="red" )
-# hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of c", xlab="True value = red line")
-# abline(v = mean(chain[-(1:burnIn),3]))
-# abline(v = true3, col="red" )
-# hist(chain[-(1:burnIn),4],nclass=30, main="Posterior of sd", xlab="True value = red line")
-# abline(v = mean(chain[-(1:burnIn),4]) )
-# abline(v = trueSD, col="red" )
-# 
-# plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a", )
-# abline(h = true1, col="red" )
-# plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b", )
-# abline(h = true2, col="red" )
-# plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of c", )
-# abline(h = true3, col="red" )
-# plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd", )
-# abline(h = trueSD, col="red" )
-# 
+
 filename <- paste0("~/MAD_MODEL/SUR_MODEL/Code/chain_IC_0_03_var_0_0008_MH_2000eq_3param_",iterations,"_",Sys.Date(),".RData") #Salva cada ronda de optimizaciones, por si acaso
 save(chain, file = filename)
 
