af713cd667767efb75bc0ec2d2b9d48068405dd2
mpardo1 <pardoaraujomarta92@gmail.com>
2021-11-02 13:32:13 +0100

    last changes

--- MAD_MODEL/RStudio/Life_table_lx/Segmented_MAD_mod_1.R
+++ MAD_MODEL/RStudio/Life_table_lx/Segmented_MAD_mod_1.R
@@ -6,7 +6,7 @@ library(ggplot2)
 
 
 # Read data from de csv and save it on life_table:
-Path = "/home/marta/Documentos/PHD/2021/SUR_Model/John_Palmer/Life_table/participation_life_table.csv"
+Path = "/Users/celsaaraujobarja/MAD_MODEL/DATA_PROCESS/Data/participation_life_table.csv"
 life_table = read.csv(Path) 
 
 # Segmented does not allow referenced to the dataframe class
@@ -30,7 +30,7 @@ for(i in 1:10){
 plot(nod_vec, rSq_ve, xlab = "Number of breakpoints", ylab = "R squared", xlim = c(0,10))
 
 # Do segmented for 5 breakpoints since it appears to be the most efficient number of breakpoints.
-num_breakp = 2
+num_breakp = 4
 #seg_5 <- segmented(LinearMod, seg.Z = ~x, control=seg.control( fix.npsi=FALSE))
 seg_5 <- segmented(LinearMod, seg.Z = ~x, npsi = num_breakp)
 fit_data2 <- fitted(seg_5)
@@ -57,3 +57,4 @@ model <- data.frame(Age = x, Participants = fit_data2)
 ggplot(model, aes(x = Age, y = Participants)) + geom_line() 
 ggplot(model, aes(x = Age, y = Participants)) + geom_line() + geom_point(data = life_table, aes(x = x, y = lx), color = "red",legend.title = " nº breakpoints = 6")
 ggplot(model, aes(x = Age, y = log10(Participants)))+ geom_line() + geom_point(data = life_table, aes(x = x, y = log10(lx)), color = "red")
+
